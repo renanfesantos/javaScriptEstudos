2eb0f38348a9b885d0c5f0e20bb0cd84f0c77a94
User <User@DESKTOP-0NI9S3J>
2018-11-01 15:17:09 -0300

    atualização

--- EstudoJavaScript/EstudoJavaScriptCodigo.js
+++ EstudoJavaScript/EstudoJavaScriptCodigo.js
@@ -3,5 +3,22 @@
  */
 
 function myFunction() {
-   document.getElementById("subTitulo").innerHTML = "Mudei o subtitulo com script externo";
+	document.getElementById("subTitulo").innerHTML = "Mudei o subtitulo com script externo";
+}
+
+function dadosComInnerHTML() {
+	document.getElementById("fraseQualquer").innerHTML = 
+		"Inserção de dados, com .innerHTML, mudei uma frase qualquer";
+}
+
+function declaracoesEmJavaScript(){
+	var x, y, z;
+	x = 5;
+	y = 5;
+	z = x + y;
+	
+	var frase = "Teste de declaração de variáveis. 5 + 5 = ";
+	
+	window.alert(frase + z);
+
 }
